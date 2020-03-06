f62abd6d1cf227ca1f787def1f5cb5032549818f
Shantanu Laghate <shon615@gmail.com>
2020-03-05 23:58:13 -0500

    Smartbox code threaded

---
+++ shantanu-code/camera-test-syscall.py
@@ -0,0 +1,51 @@
+import sys
+from matplotlib import pyplot as plt
+import time
+import base64
+import numpy as np
+import requests
+from requests.exceptions import HTTPError
+import json
+import subprocess
+import PIL
+
+camera_port = 1
+
+start = time.time()
+ret = subprocess.run(["fswebcam", "/home/pi/smart-box/shantanu-code/pics/test.jpg",
+                      "-q", "-r", "640x480", "--no-banner", "--skip", "2", "--set", "brightness=50%"])
+end = time.time()
+
+print(ret)
+
+image = np.asarray(PIL.Image.open("/home/pi/smart-box/shantanu-code/pics/test.jpg"))
+
+b64im = base64.b64encode(image)
+image_encoded = b64im.decode('utf-8')
+
+print("Took", end-start)
+#print("base64", b64im)
+print("len", len(image_encoded))
+print("type", type(image_encoded))
+
+
+#frameRGB = image[:,:,::-1] # BGR => RGB
+#plt.imshow(frameRGB)
+#plt.show()
+
+
+"""
+
+# change this when server is hosted.
+url = "http://127.0.0.1:5000"
+headers = {'content-type': 'application/json'}
+post = {'box_name': "Box0", 
+        'time': np.rint(time.time(), casting="safe"),
+        'picture': image_encoded}
+
+try:
+    r = requests.post(url+"/picture_entry", data=json.dumps(post), headers=headers)
+    r.raise_for_status()
+except HTTPError as http_err:
+    print(f'HTTP error occurred: {http_err}')  # Python 3.6
+"""
--- shantanu-code/upload_chunks_flask.py
+++ shantanu-code/upload_chunks_flask.py
@@ -12,59 +12,104 @@ import json
 import requests
 import sys
 from requests.exceptions import HTTPError
-import cv2
 import base64
-
+import threading
+import subprocess
+from PIL import Image
 
 sampling_period = 1
 
-print("initializing board")
-
-"""USE THIS FOR LAPTOP"""
-ser = serial.Serial('COM5', baudrate=115200, timeout=1)
-
-"""USE THIS FOR RASPBERRY PI"""
-#ser = serial.Serial('/dev/ttyACM0', baudrate=9600, timeout=1)
+ARDUINO_PORT = '/dev/ttyACM0' # use 'COM5' with windows
+SERVER_URL = 'http://39e5b6eb.ngrok.io'
 
 
-#===================================================
+#==================== SETUP BOARD, CONFIG, CAMERA =================
+def setup(url):
+    global ARDUINO_PORT
+    global box_name
+    global sensor_names
+    global channel_names
+    global USING_CAMERA
+    global headers
+    global ser
+    
+    print("initializing board")
 
-# allow arduino board to initialize fully before 
-# collecting any data points
-time.sleep(3)
+    """USE THIS FOR LAPTOP"""
+    #ser = serial.Serial('COM5', baudrate=115200, timeout=1)
 
-
-
-
-print("ready to collect")
-with open("config.json", 'r') as fp:
-    config = json.load(fp)
-
-sensor_names = config['channel_names']
-channel_names = [x[0] for x in config['channel_names']]
-box_name = config['box_name']
-
-# MAKE CAMERA PORT SOMETHING NEGATIVE IF NOT IN USE
-camera_port = int(config['camera_port'])
-
-if camera_port >= 0:
-    USING_CAMERA = True
-else:
-    USING_CAMERA = False
-
-if USING_CAMERA:
-    camera = cv2.VideoCapture(camera_port,cv2.CAP_DSHOW)
-    # Check if the webcam is opened correctly
-    if not camera.isOpened():
-        raise IOError("Cannot open webcam")
+    """USE THIS FOR RASPBERRY PI"""
+    ser = serial.Serial(ARDUINO_PORT, baudrate=115200, timeout=1)
 
 
-# change this when server is hosted.
-url = "http://127.0.0.1:5000"
-headers = {'content-type': 'application/json'}
+    #===================================================
+
+    # allow arduino board to initialize fully before 
+    # collecting any data points
+    time.sleep(3)
 
 
 
+
+    print("configuring settings from json file")
+    with open("config.json", 'r') as fp:
+        config = json.load(fp)
+    
+    sensor_names = config['channel_names']
+    channel_names = [x[0] for x in config['channel_names']]
+    box_name = config['box_name']
+    
+    
+    # MAKE USING_CAMERA 0 if not in use.
+    USING_CAMERA = bool(config['USING_CAMERA'])
+
+    if USING_CAMERA:
+        print("Using Camera - make sure its plugged in")
+
+    
+    headers = {'content-type': 'application/json'}
+    
+    print("server url: ", url)
+    
+    check_box()
+
+
+def check_box():
+    """
+    #CHECK IF THE DATABASE HAS THE BOXNAME AND ALL THE CHANNEL NAMES HERE
+    """
+
+    post = {'box_name': box_name, 
+            'channel_names': sensor_names}
+
+    try: 
+        print("Sending request")
+        r = requests.post(SERVER_URL+"/check_box", data=json.dumps(post), headers=headers)
+        r.raise_for_status()
+
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')  # Python 3.6
+        ser.close()
+    except Exception as err:
+        print(f'Other error occurred: {err}')  # Python 3.6
+        ser.close()
+    else:
+        print('Success!')
+        print("BOX VALIDATED!")
+
+
+
+
+setup(SERVER_URL)
+
+
+
+
+
+
+
+
+#===========================GET DATA FROM ARDUINO================
 def get_values():
     arduinoData = ""
     try:
@@ -79,43 +124,60 @@ def get_values():
     print("data: ", arduinoData)
     return arduinoData.strip()
 
-def get_picture_base64():
-    return_value = False
-    while not return_value:
-        return_value, image = camera.read()
-    
+#========================TAKING AND ENCODING PICTURES ============================
+def get_picture_base64(pic_as_array):
     # length 1,228,800 base 64 string to be sent in post request
-    b64im = base64.b64encode(image)
+    b64im = base64.b64encode(pic_as_array)
     image_encoded = b64im.decode('utf-8')
 
     print("created base64 image len", len(image_encoded))
     return image_encoded
 
+# returns array with 10 base64 pictures
+def take_10_pictures(imagesb64):
+    
+    for i in range(11):
+        ret = subprocess.run(["fswebcam", "/home/pi/smart-box/shantanu-code/pics/pic{}.jpg".format(i),
+                      "-q", "-r", "640x480", "--no-banner", "--skip", "2", "--set", "brightness=50%"])
+    
+    for i in range(11):
+        image = np.asarray(Image.open("/home/pi/smart-box/shantanu-code/pics/pic{}.jpg".format(i)))
+        imagesb64.append(get_picture_base64(image))
+        
+    return imagesb64
 
-
-"""
-#CHECK IF THE DATABASE HAS THE BOXNAME AND ALL THE CHANNEL NAMES HERE
-"""
-
-post = {'box_name': box_name, 
-        'channel_names': config['channel_names']}
-
-try: 
-    print("Sending request")
-    r = requests.post(url+"/check_box", data=json.dumps(post), headers=headers)
-    r.raise_for_status()
-
-except HTTPError as http_err:
-    print(f'HTTP error occurred: {http_err}')  # Python 3.6
-    ser.close()
-except Exception as err:
-    print(f'Other error occurred: {err}')  # Python 3.6
-    ser.close()
-else:
-    print('Success!')
-    print("BOX VALIDATED!")
-
-
+#==================SEND DATA TO SERVER=========================
+def send_data(datapoints, picture_thread, picturesb64):
+    post = {}
+    post["box_name"] = box_name
+    post["channel_names"] = channel_names
+    post['datapoints'] = datapoints
+    
+    if USING_CAMERA:
+        picture_thread.join()
+        pictures = {}
+        for i, ts in enumerate(datapoints.keys()):
+            print(i, ts)
+            pictures[ts] = picturesb64[i]
+        
+        post['pictures'] = pictures
+        
+    
+    print(post.keys())
+    
+    try:
+        r = requests.post(SERVER_URL+"/data_entry", data=json.dumps(post), headers=headers)
+        r.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')  # Python 3.6
+        ser.close()
+    else:
+        print("Data Submitted successfully!")
+    
+    print("Done!")
+    
+    
+    
 
 """""
 NOW THE FUN STARTS! LET'S COLLECT DATA!!
@@ -125,18 +187,16 @@ NOW THE FUN STARTS! LET'S COLLECT DATA!!
 # collect data for 100 seconds - 10 chunks of 10 seconds each.
 
 
-for _ in range(2):
+for _ in range(10):
     # run script for 10 seconds
     endtime = time.time() + 10
     
-    post = {}
-    post["box_name"] = box_name
-    post["channel_names"] = channel_names
+    if USING_CAMERA:
+        imagesb64 = []
+        pictures_thread = threading.Thread(target=take_10_pictures, args=(imagesb64,))
+        pictures_thread.start()
 
     datapoints = {}
-
-    pictures = {}
-
     
     #rec = np.zeros(shape=(0, len(channel_names)), dtype=int)
     
@@ -148,34 +208,19 @@ for _ in range(2):
         data = get_values()
         
         # append the sensor values to rec, and time stamp to ts
-        #data_floats = np.array(list(map(float, data.split(",")))).reshape((1,len(channel_names)))
+        # data_floats = np.array(list(map(float, data.split(",")))).reshape((1,len(channel_names)))
         # do this on the server side!
-        #datapoints[t] = data_floats
+        # datapoints[t] = data_floats
         datapoints[t] = data
         
-        if USING_CAMERA:
-            pictures[t] = get_picture_base64()
 
         while time.time() < t + sampling_period:
             time.sleep(0.05)
-
-    post["datapoints"] = datapoints
-    # if we took pictures
-    if pictures:
-        post['pictures'] = pictures
-    
-    
-
-    #print(post)
-    try:
-        r = requests.post(url+"/data_entry", data=json.dumps(post), headers=headers)
-        r.raise_for_status()
-    except HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')  # Python 3.6
-        ser.close()
+            
+    if USING_CAMERA:
+        send_thread = threading.Thread(target=send_data, args=(datapoints, pictures_thread, imagesb64))
     else:
-        print("Data Submitted successfully!")
+        send_thread = threading.Thread(target=send_data, args=(datapoints, None, None))
+    send_thread.start()
+    
 
-    print("Done!")
-
-        
